f5cbd15e3e954f5dde52be3cc74555ee0034e468
Savanna Rovira <savanna.rovira@gmail.com>
2016-03-15 19:05:15 -0400

    Added models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+
+root 'welcome#index'
+  resources :categories, only: [:show]
+  resources :classifieds, only: [:new, :create, :index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
