5659fcd1170ecba49aac13ab49652d1bf833b3fb
Savanna Rovira <savanna.rovira@gmail.com>
2016-03-15 20:19:14 -0400

    Adds create to classifieds and also fixes a couple bugs

--- app/controllers/classifieds_controller.rb
+++ app/controllers/classifieds_controller.rb
@@ -6,7 +6,30 @@ class ClassifiedsController < ApplicationController
 
 
   def new
+    @classified = Classified.new
+    @classified.build_category
   end
+
+  def create
+    @classified = Classified.new(safe_classified_params)
+    @classified.build_category(safe_category_params)
+
+    if @classified.save
+      redirect_to classifieds_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def safe_classified_params
+    params.require(:classified).permit(:title, :price, :description)
+  end
+
+  def safe_category_params
+    params.require(:category).permit(:name)
+  end
+
 
 
 end
